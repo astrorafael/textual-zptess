ffeffc00e42baf5751359f903dde2f370171ded7
Rafael Gonzalez <astrorafael@yahoo.es>
2024-05-28 10:34:52 +0200

    added TCP Transport

--- src/zptess/photometer/protocol/transport.py
+++ src/zptess/photometer/protocol/transport.py
@@ -22,10 +22,6 @@ from pubsub import pub
 # local imports
 # -------------
 
-
-from zptess import __version__
-from zptess.main import arg_parser, configure_logging
-
 # ----------------
 # Module constants
 # ----------------
@@ -34,7 +30,6 @@ from zptess.main import arg_parser, configure_logging
 # Module global variables
 # -----------------------
 
-# get the root logger
 log = logging.getLogger(__name__)
 
 # -------------------
@@ -43,23 +38,40 @@ log = logging.getLogger(__name__)
 
 class UDPTransport:
 
-	def __init__(self, host='0.0.0.0', port=2255):
-		self.local_host = host
-		self.local_port = port
+    def __init__(self, host='0.0.0.0', port=2255):
+        self.local_host = host
+        self.local_port = port
 
-	async def readings(self):
-	    async with await anyio.create_udp_socket(
-	        family = socket.AF_INET, 
-	        local_host = self.local_host,
-	        local_port = self.local_port
-	    ) as udp:
-	        async for payload, (host, port) in udp:
-	        	now = datetime.datetime.now(datetime.timezone.utc)
-	        	pub.sendMessage('reading', timestamp=now, payload=payload)
-	        	log.info("%s => %s", now, payload)  
+    async def readings(self):
+        async with await anyio.create_udp_socket(
+            family = socket.AF_INET, 
+            local_host = self.local_host,
+            local_port = self.local_port
+        ) as udp:
+            async for payload, (host, port) in udp:
+                now = datetime.datetime.now(datetime.timezone.utc)
+                pub.sendMessage('reading', timestamp=now, payload=payload)
+                log.info("%s => %s", now, payload)  
+
+
+class TCPTransport:
+
+    def __init__(self, host="192.168.4.1", port=23):
+        self.host = host
+        self.port = port
+
+    async def readings(self):
+        async with await anyio.connect_tcp(
+            remote_host = self.host, 
+            remote_port = self.port 
+        ) as tcp:
+            async for payload in tcp:
+                now = datetime.datetime.now(datetime.timezone.utc)
+                pub.sendMessage('reading', timestamp=now, payload=payload)
+                log.info("%s => %s", now, payload)  
 
 
 class SerialTransport:
-	pass
+    pass
 
 
