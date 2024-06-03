207976610bb2f73c3c2bbceff9561272bf07e60a
Rafael Gonzalez <astrorafael@gmail.com>
2024-06-03 17:03:20 +0200

    refactored controller

--- src/zptess/controller.py
+++ src/zptess/controller.py
@@ -48,44 +48,31 @@ log = logging.getLogger()
 
 def handle_error(excgroup: ExceptionGroup) -> None:
     for exc in excgroup.exceptions:
-        logging.error(exc)
-
-
-
-           
+        log.error(exc)
 
 class Controller:
 
     def __init__(self, tui):
         self.tui = tui
-
         self.send_stream1, self.receive_stream1 = anyio.create_memory_object_stream[dict](max_buffer_size=4)
         self.ref_photometer = Photometer(role='ref', old_payload=True, stream=self.send_stream1)
-
         self.send_stream2, self.receive_stream2 = anyio.create_memory_object_stream[dict](max_buffer_size=4)
         self.test_photometer =  Photometer(role='test', old_payload=False, stream=self.send_stream2)
 
     async def receptor(self, role, stream):
-        log = logging.getLogger(label(role))
-        widget = self.tui.get_log_widget(role)
-        async with stream:
-            async for i, message in a.enumerate(stream, start=1):
-                widget.write_line(str(message))
-
-    async def get_info(self):
+        photometer = self.ref_photometer if role == 'ref' else  self.test_photometer
         try:
-            info = await self.ref_photometer.get_info()
-            self.tui.update_metadata('ref', info)
-            info = await self.test_photometer.get_info()
-            log.info(info)
-            self.tui.update_metadata('test', info)
+            info = await photometer.get_info()
+            self.tui.update_metadata(role, info)
         except Exception as e:
             log.error(e)
-
+        else:
+            widget = self.tui.get_log_widget(role)
+            async with stream:
+                async for i, message in a.enumerate(stream, start=1):
+                    widget.write_line(str(message))
 
     async def run_async(self):
-
-        await self.get_info()
 
         # Catching exception groups this way is pre-Python 3.11
         with catch({
@@ -94,7 +81,7 @@ class Controller:
             #anyio.BrokenResourceError: handle_error,
         }):
             async with anyio.create_task_group() as tg:
-                tg.start_soon(self.ref_photometer.readings)
                 tg.start_soon(self.test_photometer.readings)
-                tg.start_soon(self.receptor, 'ref', self.receive_stream1)
+                tg.start_soon(self.ref_photometer.readings)
                 tg.start_soon(self.receptor, 'test', self.receive_stream2)
+                tg.start_soon(self.receptor, 'ref', self.receive_stream1)
