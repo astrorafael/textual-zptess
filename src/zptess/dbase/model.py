8b91a19f84bdee6503c44377f87e332a0de14c70
Rafael Gonzalez <astrorafael@gmail.com>
2024-07-08 13:00:26 +0200

    added summary table to the model

--- src/zptess/dbase/model.py
+++ src/zptess/dbase/model.py
@@ -1,42 +1,42 @@
-# ----------------------------------------------------------------------
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
 # Copyright (c) 2024 Rafael Gonzalez.
 #
 # See the LICENSE file for details
-# ----------------------------------------------------------------------
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
 
 
-#--------------------
+## # # # # # # # # # 
 # System wide imports
-# -------------------
+# # # # # # # # # # -
 
 import logging
 
 from typing import Optional, List
 from datetime import datetime
 
-# ---------------------
+# # # # # # # # # # # -
 # Third party libraries
-# ---------------------
+# # # # # # # # # # # -
 
 from sqlalchemy import Table, Column, Integer, String, DateTime, ForeignKey, UniqueConstraint, PrimaryKeyConstraint
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from lica.sqlalchemy.asyncio.dbase import Model
 
-# ----------------
+# # # # # # # # # 
 # Module constants
-# ----------------
+# # # # # # # # # 
 
-# -----------------------
+# # # # # # # # # # # # -
 # Module global variables
-# -----------------------
+# # # # # # # # # # # # -
 
 # get the module logger
 log = logging.getLogger(__name__)
 
-# ---------------------------------
+# # # # # # # # # # # # # # # # # -
 # Data Model, declarative ORM style
-# ---------------------------------
+# # # # # # # # # # # # # # # # # -
 
 class Config(Model):
 
@@ -62,7 +62,7 @@ class Photometer(Model):
     id:             Mapped[int] = mapped_column(primary_key=True)
     name:           Mapped[str] = mapped_column(String(10))
     mac:            Mapped[str] = mapped_column(String(17))
-    sensor:         Mapped[str] = mapped_column(String(12))
+    sensor:         Mapped[str] = mapped_column(String(12)) # Sensor model: TSL237, S9705-01DT
     model:          Mapped[str] = mapped_column(String(8))
     firmware:       Mapped[str] = mapped_column(String(17))
     zero_point:     Mapped[float]
@@ -70,6 +70,8 @@ class Photometer(Model):
 
     # This is not a real column, it s meant for the ORM
     samples: Mapped[List['Sample']] = relationship(back_populates="photometer")
+    # This is not a real column, it s meant for the ORM
+    calibrations: Mapped[List['Summary']] = relationship(back_populates="photometer")
 
     def __repr__(self) -> str:
         return f"TESS(id={self.id!r}, name={self.name!r}, mac={self.mac!r})"
@@ -123,9 +125,14 @@ class Round(Model):
     seq:        Mapped[int] = mapped_column('round', Integer) # Round number form 1..NRounds
     role:       Mapped[str] = mapped_column(String(4))
     session:    Mapped[int] = mapped_column(Integer)
-    freq:       Mapped[Optional[float]] # Average of Median method
+    freq:       Mapped[Optional[float]]         # Average of Median method
     central:    Mapped[Optional[str]] = mapped_column(String(6))  # ether 'mean' or 'median'
-    stddev:     Mapped[Optional[float]] # Standard deviation for frequency central estimate
+    stddev:     Mapped[Optional[float]]         # Standard deviation for frequency central estimate
+    mag:        Mapped[Optional[float]]         # magnitiude corresponding to central frequency and summing ficticious zero point 
+    zp_fict:    Mapped[Optional[float]]         # Ficticious ZP to estimate instrumental magnitudes (=20.50)
+    zero_point: Mapped[Optional[float]]         # Estimated Zero Point for this round ('test' photometer round only, else NULL)
+    nsamples:   Mapped[Optional[int]]           # Number of samples for this round
+    duration:   Mapped[Optional[float]]         # Approximate duration, in seconds
 
     # samples per round. Shoudl match the window size
     # This is not a real column, it s meant for the ORM
@@ -137,6 +144,43 @@ class Round(Model):
             seq,
             role),
         {})
+
+
+class Summary(Model):
+    __tablename__ = "summary_t"
+
+    id:             Mapped[int] = mapped_column(primary_key=True)
+    phot_id:        Mapped[int] = mapped_column(ForeignKey("photometer_t.id"), index=True)
+
+    session:        Mapped[int] = mapped_column(Integer)                # calibration session identifier
+    role:           Mapped[str] = mapped_column(String(4))              # either 'test' or 'ref'
+    calibration:    Mapped[Optional[str]] = mapped_column(String(6))    # Either 'MANUAL' or 'AUTO'
+    calversion:     Mapped[Optional[str]] = mapped_column(String(64))   # calibration software version
+    prev_zp:        Mapped[Optional[float]]                             # previous ZP before calibration
+    author:         Mapped[Optional[str]]                               # who run the calibration
+    nrounds:        Mapped[Optional[int]]                               # Number of rounds passed
+    offset:         Mapped[Optional[float]]                             # Additional offset that was summed to the computed zero_point
+    upd_flag:       Mapped[Optional[bool]]                              # 1 => TESS-W ZP was updated, 0 => TESS-W ZP was not updated
+    zero_point:         Mapped[Optional[float]]                         #  calibrated zero point
+    zero_point_method:  Mapped[Optional[str]]  = mapped_column(String(6))  #  either the 'mode' or 'median' of the different rounds
+    freq:               Mapped[Optional[float]]                            # final chosen frequency
+    freq_method:        Mapped[Optional[str]]  = mapped_column(String(6))  #  either the 'mode' or 'median' of the different rounds
+    mag:                Mapped[Optional[float]]                            #  final chosen magnitude uzing ficticious ZP
+    filter:             Mapped[Optional[str]] = mapped_column(String(32))  #  Filter type (i.e. UV-IR/740)
+    plug:               Mapped[Optional[str]] = mapped_column(String(16))  #  Plug type (i.e. USB-A)
+    box:                Mapped[Optional[str]] = mapped_column(String(16))  #  Box model (i.e. FSH714)
+    collector:          Mapped[Optional[str]] = mapped_column(String(16))  #  Collector model
+    comment:            Mapped[Optional[str]] = mapped_column(String(255)) #  Additional comment for the callibration process
+
+    # This is not a real column, it s meant for the ORM
+    photometer: Mapped['Photometer'] = relationship(back_populates="calibrations")
+
+    __table_args__ = (
+        UniqueConstraint(
+            session,
+            role),
+        {})
+
 
 class Batch(Model):
     __tablename__ = "batch_t"
